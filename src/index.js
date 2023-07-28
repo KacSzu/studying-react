ce6b7979f2549facac39533e6f8120fc52b8a594
KacSzu <szulckacper19@gmail.com>
2023-07-28 13:13:17 +0200

    React Fragments

--- src/index.js
+++ src/index.js
@@ -65,24 +65,28 @@ function Header() {
 }
 function Menu() {
   const pizzas = pizzaData;
-
+  // const pizzas = [];
   const numPizzas = pizzas.length;
   return (
     <main className="menu ">
       <h2> Our Menu</h2>
 
       {numPizzas > 0 ? (
-        <ul className="pizzas">
-          {pizzas.map((pizza) => {
-            return <Pizza pizzaObj={pizza} key={pizza.name} />;
-          })}
-        </ul>
+        <>
+          <p>Authentic italian pizza</p>
+          <ul className="pizzas">
+            {pizzas.map((pizza) => {
+              return <Pizza pizzaObj={pizza} key={pizza.name} />;
+            })}
+          </ul>
+        </>
       ) : (
         <p>We're still working on our menu. Please come back later</p>
       )}
     </main>
   );
 }
+
 function Pizza({ pizzaObj }) {
   if (pizzaObj.soldOut) return null;
   return (
